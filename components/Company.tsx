66a4ec343065450e0729d595ca0c15a41919c977
Adam Skrabania <adam.skrabania@protonmail.com>
2024-08-05 19:23:33 +0200

    next one

--- components/Company.tsx
+++ components/Company.tsx
@@ -2,8 +2,12 @@
 import Image from 'next/image';
 import styles from '@/styles/Company.module.css'
 import biuro from '@/public/Biuro Inżynierskie Jerzy Skrabania.svg';
+import obraz from '@/public/obraz.jpg'
 import React from 'react';
 import {motion} from "framer-motion";
+import Link from 'next/link';
+import {BsArrowUpRight} from "react-icons/bs";
+
 
 export default function Company() {
   return (
@@ -19,10 +23,22 @@ export default function Company() {
                 />
             </motion.div>
         </div>
-        <div className={styles}>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className={styles.grid1}>
+            <span className={styles.text1}>Usługi inżynierskie w zakresie automatyki przemysłowej</span>
+            <span className={styles.text2}>Z naszych  usług korzystają największe koncerny w Polsce</span>
+            <div>
+              <Link href="#kontakt" className={styles.kontakt2}
+              >Kontakt <BsArrowUpRight />
+              </Link>
+            </div>
+            <div className={styles.obraz}>
+              <Image
+                src={obraz}
+                alt='Biuro'
+                quality="95"
+                priority= {true}
+              />
+            </div>
         </div>
     </section>
   )

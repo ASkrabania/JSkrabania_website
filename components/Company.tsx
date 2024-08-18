2098b64a71c7798921aaa70971e2fe2749fd0d51
Adam Skrabania <adam.skrabania@protonmail.com>
2024-08-18 22:00:18 +0200

    Next one

--- components/Company.tsx
+++ components/Company.tsx
@@ -12,32 +12,35 @@ import {BsArrowUpRight} from "react-icons/bs";
 export default function Company() {
   return (
     <section>
-        <div className={styles.biuro_zew}>
+        <div className={styles.nazwa}>
             <motion.div>
-                <Image 
+              <h1>Biuro Inżynierskie <br /> Jerzy Skrabania</h1>
+                {/* <Image 
                 
                 src={biuro}
                 alt='Jerzy Skrabania'
                 quality="95"
                 priority={true}
-                />
+                /> */}
             </motion.div>
         </div>
         <div className={styles.hero}>
             <div className={styles.info}>
-                <span className={styles.text1}>Usługi inżynierskie w zakresie automatyki przemysłowej</span>
-                <span className={styles.text2}>Z naszych  usług korzystają największe koncerny w Polsce</span>
+                <span className={styles.text1}>
+                  Usługi inżynierskie w zakresie automatyki przemysłowej</span>
+                <span className={styles.text2}>
+                  Z naszych  usług korzystają największekoncerny w Polsce</span>
                 <Link href="#kontakt" className={styles.kontakt2}
                 >Kontakt <BsArrowUpRight />
                 </Link>
             </div>
             <div className={styles.obraz}>
-              {/* <Image
+              <Image
                 src={obraz}
                 alt='Biuro'
                 quality="95"
                 priority= {true}
-              /> */}
+              />
             </div>
         </div>
     </section>
